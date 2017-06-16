ed77d21d1dfc5121a69c7203a2f4a1ac060b6388
Leonardo Sarmento de Castro <leonardo.sarmentocastro@gmail.com>
2017-06-16 20:14:12 -0300

    Linking to routes using via template
    Details
    . Added property "[routerLink]" to enable navigation between routes when clicking elements on the template.

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -3,7 +3,9 @@ import {Component, Input} from '@angular/core';
 @Component({
     selector: 'event-thumbnail',
     template: `
-        <div class='well hoverwell thumbnail'>
+        <div
+            [routerLink]='["/events", event.id]' 
+            class='well hoverwell thumbnail'>
             <h2>{{event?.name}}</h2>
             
             <div>Date: {{event?.date}}</div>
