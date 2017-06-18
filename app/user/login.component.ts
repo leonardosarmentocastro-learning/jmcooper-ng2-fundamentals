37c8b47c3010438724d820d739bb6abec9a4046e
Leonardo Sarmento de Castro <leonardo.sarmentocastro@gmail.com>
2017-06-18 15:01:17 -0300

    Validating templated-based forms
    Details
    . Used "form.controls.<input-name>" in order to validate if a given  "required" field was properly filled or not;
    . Implemented two ways of doing the previously validation: putting the "*ngIf" logic on the template and leaving the logic on the "login.component.ts"

--- app/user/login.component.ts
+++ app/user/login.component.ts
@@ -10,23 +10,44 @@ import {Router}     from '@angular/router';
 import {AuthService} from './index';
 
 @Component({
-    templateUrl: 'app/user/login.template.html'
+    templateUrl: 'app/user/login.template.html',
+    styles: [`
+        em { float:right; color: #E05C65; padding-left: 10px;}
+    `]
 })
 export class LoginComponent {
+    mouseoverLogin:boolean;
     constructor(
         private authService:AuthService,
         private router:Router) {
 
     }
     
+    cancel() {
+        let route = ['events'];
+        this.router.navigate(route);
+    }
+
+    /**
+     * Validating if a given input is valid by code instead of doing it directly on the template.
+     * @param input
+     */
+    hasToShowInputErrorMessage(input) {
+        let hasToShowInputErrorMessage = true;
+        if (input) {
+            let mouseoverLogin = this.mouseoverLogin;
+            if (input.invalid && (input.touched || mouseoverLogin)) 
+                return hasToShowInputErrorMessage;
+
+            return !hasToShowInputErrorMessage;
+        }
+
+        return !hasToShowInputErrorMessage;
+    }
+
     login(loginForm) {
         let userName = loginForm.userName;
         let password = loginForm.password;
         this.authService.login(userName, password);
-    }
-
-    cancel() {
-        let route = ['events'];
-        this.router.navigate(route);
     }
 }
