71cde5b55bb72203e6f16eb981bd0a9c53711fe7
Leonardo Sarmento de Castro <leonardo.sarmentocastro@gmail.com>
2017-06-18 20:49:20 -0300

    Validating "reactive forms"
    Details
    . Used "Validators" to added validations on the "reactive form" of "profile.component".

--- app/user/profile.component.ts
+++ app/user/profile.component.ts
@@ -1,9 +1,9 @@
 /**
  * NPM packages.
  */
-import {Component, OnInit}        from '@angular/core';
-import {FormControl, FormGroup}   from '@angular/forms';
-import {Router}                   from '@angular/router';
+import {Component, OnInit}                    from '@angular/core';
+import {FormControl, FormGroup, Validators}   from '@angular/forms';
+import {Router}                               from '@angular/router';
 
 /**
  * Project packages.
@@ -11,9 +11,16 @@ import {Router}                   from '@angular/router';
 import {AuthService} from './index';
 
 @Component({
-  templateUrl: 'app/user/profile.template.html' 
+  templateUrl: 'app/user/profile.template.html',
+  styles: [`
+    em { float: right; color: #E05C65; padding-left: 10px; }
+    .error input {background-color: #E3C3C5;}
+    .error ::-webkit-input-placeholder {color: #999;}
+  `]
 })
 export class ProfileComponent implements OnInit {
+  firstName:FormControl;
+  lastName:FormControl;
   profileForm:FormGroup;
 
   constructor(
@@ -31,17 +38,22 @@ export class ProfileComponent implements OnInit {
     // }
 
     if (currentUser) {
-      let firstName     = new FormControl(currentUser.firstName);
-      let lastName      = new FormControl(currentUser.lastName);
+      this.firstName = new FormControl(currentUser.firstName, Validators.required);
+      this.lastName  = new FormControl(currentUser.lastName, Validators.required);
+      
       this.profileForm  = new FormGroup({
-        firstName,
-        lastName
+        firstName: this.firstName,
+        lastName: this.lastName
       });
     }
   }
 
+  cancel() {
+    let route = ['events'];
+    this.router.navigate(route);
+  }
+
   saveProfile(profileForm) {
-    
     let values    = profileForm.value;
     let firstName = values.firstName;
     let lastName  = values.lastName;
@@ -51,8 +63,13 @@ export class ProfileComponent implements OnInit {
     this.router.navigate(route);
   }
 
-  cancel() {
-    let route = ['events'];
-    this.router.navigate(route);
+  validateFirstName() {
+    let isValid = this.firstName.valid || this.firstName.untouched;
+    return isValid;
+  }
+
+  validateLastName() {
+    let isValid = this.lastName.valid || this.lastName.untouched;
+    return isValid;
   }
 }
